01f6b4bef0fabc8546ec42ddc4beb024101b40a2
AtheoJester23 <atheojester@gmail.com>
2026-01-30 02:43:21 +0800

    fix: contact content

--- src/components/Home_Sections/Contact.tsx
+++ src/components/Home_Sections/Contact.tsx
@@ -1,7 +1,16 @@
 const Contact = () => {
     return (  
-        <div className="bg-[rgb(8,8,8)] h-[100vh] flex p-20 justify-center items-center text-white">
-            <h1 className="text-5xl font-bold">Contact</h1>
+        <div className="bg-[rgb(8,8,8)] h-[100vh] flex flex-col gap-2 p-20 justify-center items-center text-white">
+            <h1 className="text-5xl font-bold">Let's collaborate...</h1>
+            <p>atheojester@gmail.com</p>
+            <div className="flex gap-2 mt-5">
+                <a href="https://www.linkedin.com/in/atheojester23/" target="_blank" className="hover:-translate-y-1 duration-300">
+                    <i className="devicon-linkedin-plain text-5xl"></i>
+                </a>
+                <a href="https://github.com/AtheoJester23" target="_blank" className="hover:-translate-y-1 duration-300">
+                    <i className="devicon-github-original text-5xl"></i>
+                </a>
+            </div>
         </div>
     );
 }
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -5,7 +5,6 @@ const Test = () => {
     return (  
         <div className="h-screen w-full bg-[rgb(8,8,8)] flex justify-center items-center">
             <Cursor/>
-            <ProjectItem/>
         </div>
     );
 }
